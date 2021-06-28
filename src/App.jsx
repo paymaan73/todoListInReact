92032a432f0b48ab37102d79d5677a6b0969a08f
paymaan <paymankazemipymkz@gmail.com>
2021-06-28 13:38:54 +0430

    complate course

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Todo from './components/Todo.jsx';
+function App() {
+  return(
+    <div>
+      <h1>My Todos</h1>
+      <Todo text='Learn React' />
+      <Todo text='Learn Ruby On Rails' />
+      <Todo text='Learn Cloujer' />
+    </div>
+
+
+  );
+}
+
+export default App;
---
+++ src/components/Backdrop.jsx
@@ -0,0 +1,8 @@
+function Backdrop(props){
+    return(
+        <div className='backdrop' onClick = { props.onCancel }>
+        </div>
+    );
+}
+
+export default Backdrop;
